d031311cd994131e9f4b5cfdae0d364e1cc94ac4
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2024-07-03 17:46:24 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        String ID1;
+        Scanner s = new Scanner(System.in);
+        ID1 = s.next();
+        
+        if(ID1.equals(ID1))
+        	System.out.println(ID1 + "??!");       	
+        
+        s.close();
+    }
+}
