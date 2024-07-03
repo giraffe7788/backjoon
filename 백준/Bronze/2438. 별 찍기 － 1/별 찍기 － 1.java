687af553f642b67a2be35f53f38c8ad27a95487b
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2024-07-03 17:48:36 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+			StringTokenizer st = null;
+			
+			int repeat = Integer.parseInt(br.readLine());
+			
+			for(int i = 0; i < repeat; i++) {
+				
+				for(int j = 0; j <= i; j++) {
+					bw.write("*");
+				}
+				bw.newLine();
+			}
+			bw.flush();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
