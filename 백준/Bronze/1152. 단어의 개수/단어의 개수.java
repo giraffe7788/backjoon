0d827463b07def0380e7eca9be7ef3565e611c81
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2024-07-05 14:03:39 +0900

    [Bronze II] Title: 단어의 개수, Time: 252 ms, Memory: 18564 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -8,11 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
-
-			String[] words = br.readLine().trim().split(" ");
-			int length = words.length;
-			if(length == 1 && words[0].equals("")) length=0;
-			bw.write(length + "");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			bw.write(st.countTokens()+"");
 			bw.flush();
 			
 		} catch(Exception e) {
